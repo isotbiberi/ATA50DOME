8c95434ab4253ec2355f1b9e04d14a86cc3ec182
isotbiberi <ibaslar@yahoo.com>
2015-07-27 16:51:49 +0300

    https://github.com/isotbiberi/ATA50DOME.git

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static Renci.SshNet.SshClient ssh;
+        static Renci.SshNet.ShellStream stream;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("denemedir");
+
+            try
+            {
+                ssh = new SshClient("10.143.3.110", "root", "ismail");
+                ssh.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message + " ssh connect hatasi");
+            }
+
+/*
+            try
+            {
+                stream = ssh.CreateShellStream("xterm", 80, 50, 1024, 1024, 1024);
+                //Thread.Sleep(100);
+                stream.WriteLine("telnet localhost 6571");
+               // pictureBox1.Image = ATA50_Otomasyon.Properties.Resources.bagli;
+               // Thread.Sleep(100);
+               // kontrol();
+            }
+            catch (Exception ex)
+            {
+                Console.Write(ex.Message + " shell streamda hata var");
+            }
+            */
+            Console.ReadKey();
+        }
+    }
+}
